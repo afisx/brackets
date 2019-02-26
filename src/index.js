d444042fa3a881c458d0893325fdbba64ce08e03
afisx <alexej.moiseenko@gmail.com>
2019-02-26 17:53:11 +0200

    Implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let configLength = bracketsConfig.length;
+  let brackets = {};
+	for (let i = 0; i < configLength; i++){
+		(function(i){
+			return brackets[bracketsConfig[i][0]] = bracketsConfig[i][1];
+		})(i);
+	};
+	return checkStr(str, brackets);
+}
+
+function checkStr(str, brackets){
+
+	const strLength = str.length;
+	let cache = [],
+		char = '',
+		closed = '';
+	for (let j = 0; j < strLength; j++){
+		char = str[j];
+		closed = brackets[char];
+		if (char != closed){
+			if (closed){
+				cache.push(char);
+			} else if (cache.length == 0){
+				return false;
+			}else if (cache.length > 0) {
+				let last = cache.pop();
+				if (brackets[last] !== char){
+					return false;
+				}
+			}
+		} else {
+			if (cache.length > 0) {
+				let last = cache.pop();
+				if (brackets[last] !== char){
+					cache.push(last);
+					cache.push(char);
+				}
+			}else if (cache.length == 0){
+				cache.push(char);
+			}
+		}
+	}
+	return (cache.length == 0) ? true : false; 
 }
